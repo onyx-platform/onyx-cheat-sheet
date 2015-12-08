57ad8b2f5fc8d04b1a7c08cc740062eccfcafeb9
Michael Drogalis <mjd3089@rit.edu>
2015-12-07 18:05:12 -0800

    Bump to SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-cheat-sheet "0.8.2"
+(defproject onyx-cheat-sheet "0.8.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
