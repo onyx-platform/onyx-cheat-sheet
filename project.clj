b2f89c48865d1582ac3be56f48347af7de50e657
OnyxBot <michael.drogalis@onyxplatform.org>
2018-05-14 23:27:12 +0000

    Release version 0.13.0.0-beta1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-cheat-sheet "0.13.0.0-SNAPSHOT"
+(defproject onyx-cheat-sheet "0.13.0.0-beta1"
   :description "Cheat sheet for Onyx"
   :url "https://github.com/onyx-platform/onyx-cheat-sheet"
   :license {:name "Eclipse Public License"
@@ -16,7 +16,7 @@
                  [compojure "1.5.2"]
                  [enlive "1.1.6"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.13.0-alpha1"]
+                 [org.onyxplatform/onyx "0.13.0-beta1"]
                  [prismatic/om-tools "0.4.0"]
                  [markdown-clj "0.9.94"]
                  [org.omcljs/om "0.9.0"]
