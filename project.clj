af1f6bbf8adc75178b0239afc130bad4ce7a5a9b
OnyxBot <michael.drogalis@onyxplatform.org>
2018-10-29 03:44:34 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-cheat-sheet "0.14.0.0"
+(defproject onyx-cheat-sheet "0.14.0.1-SNAPSHOT"
   :description "Cheat sheet for Onyx"
   :url "https://github.com/onyx-platform/onyx-cheat-sheet"
   :license {:name "Eclipse Public License"
